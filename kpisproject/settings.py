ec8b6793445a09d1f24427d35ebd3795e9502da7
Sean Stoops <brutimus@gmail.com>
2014-11-17 16:35:42 -0800

    fixing settings order

--- kpisproject/settings.py
+++ kpisproject/settings.py
@@ -28,12 +28,17 @@ else:
 
 TEMPLATE_DEBUG = DEBUG
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.core.context_processors.request",
+)
+
 ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
