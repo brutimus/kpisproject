913c54e10b425685a56d637c1cce8052ff48b0e7
Sean Stoops <brutimus@gmail.com>
2014-11-17 16:54:33 -0800

    forgot urls

--- kpisproject/urls.py
+++ kpisproject/urls.py
@@ -14,6 +14,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
     url(r'^django_databrowse/(.*)', django_databrowse.site.root),
     url(r'^stories/$', 'kpisproject.analytics.views.story_overview'),
     url(
